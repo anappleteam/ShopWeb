6bc8ff8123594c5455d67067291ab4057b55d997
MuLiangyu <978226558@qq.com>
2018-07-19 22:07:59 +0800

    同步版本

--- 4396Shop/src/bupt/sse/shop/aspect/DataSourceAdvice.java
+++ 4396Shop/src/bupt/sse/shop/aspect/DataSourceAdvice.java
@@ -32,19 +32,19 @@ public class DataSourceAdvice implements MethodBeforeAdvice,ThrowsAdvice,AfterRe
 		if(masterMethod.contains(methodname)){
 			logger.info("切换到主数据库");
 			DataSourceSwitcher.setMaster();
-//		}else if(slaveMethod.contains(methodname)){
-//			logger.info("切换到从数据库");
-//			DataSourceSwitcher.setSlave();
-		}else{
+		}else if(slaveMethod.contains(methodname)){
 			logger.info("切换到从数据库");
 			DataSourceSwitcher.setSlave();
+		}else{
+			logger.info("切换到从数据库");
+			DataSourceSwitcher.setMaster();
 		}
 		
 	}
 	
 	public void afterThrowing(Method method, Object[] args, Object target, Exception ex) throws Throwable {
 		
-		//DataSourceSwitcher.setSlave();
+		DataSourceSwitcher.setSlave();
 		logger.info("出现异常,切换到: slave");
 	}
 
